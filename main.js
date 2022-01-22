440c66365d29edd79eedbf629c694ef837a79028
maximdegeyter <maximdegeyter@outlook.com>
2022-01-22 22:21:48 +0100

    fix assets import

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
+import backgroundImg from './assets/background.jpg';
+import sunTexture from './assets/sunTexture.jpg';
+import texture0 from './assets/texture0.jpg';
+import texture1 from './assets/texture1.jpg';
+import texture2 from './assets/texture2.jpg';
+import texture3 from './assets/texture3.jpg';
+import texture4 from './assets/texture4.jpg';
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -40,10 +48,10 @@ const addStar = () => {
 
 Array(600).fill().forEach(addStar);
 
-const spaceTexture = new THREE.TextureLoader().load('background.jpg');
+const spaceTexture = new THREE.TextureLoader().load(backgroundImg);
 scene.background = spaceTexture;
 
-const sunTexture = new THREE.TextureLoader().load('sunTexture.jpg');
+const sunTexture = new THREE.TextureLoader().load(sunTexture);
 
 const sun = new THREE.Mesh(
   new THREE.SphereGeometry(16, 30, 30),
@@ -61,9 +69,10 @@ let planet3;
 let planet4;
 
 const planets = [planet0,planet1,planet2,planet3,planet4];
+const textures = [texture0,texture1,texture2,texture3,texture4];
 
 for(let i=0; i<5; i++) {
-  const texture = new THREE.TextureLoader().load(`texture${i}.jpg`);
+  const texture = new THREE.TextureLoader().load(textures[i]);
   planets[i] = new THREE.Mesh( 
     new THREE.SphereGeometry((Math.random() * 5) + 1.5, 30, 30),
     new THREE.MeshStandardMaterial({
